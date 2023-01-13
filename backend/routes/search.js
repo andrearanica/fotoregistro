f79c80ef21d361e3fc3d42a41dcd5f8dda9e9b03
Andrea Ranica <andrearanica2004@gmail.com>
2023-01-13 09:20:53 +0100

    🎨 Return to old version

--- backend/routes/search.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { showInfo, getRole } from '../controllers/search.js'
-
-const router = express.Router()
-
-router.post('/student/:email', showInfo)
-router.post('/student/role', getRole)
-
-export default router
--- backend/routes/upload.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { uploadFile } from '../controllers/upload.js'
-
-const router = express.Router()
-
-router.post('/upload', uploadFile)
-
-export default router
